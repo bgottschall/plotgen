750736793a49c8f19a409d5490c1bc11bae408ee
Björn Gottschall <github.mail@bgottschall.de>
2021-09-06 15:00:37 +0200

    * bugfix splitting of dataframes + added some new violin modes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='plotgen',
-    version='0.4.0',
+    version='0.4.1',
     scripts=['plotgen'],
     author="Björn Gottschall",
     author_email="github.mail@bgottschall.de",
